d604a1286d7c68b9a15315b70ce86aa26f7c1c8b
archivojab@gmail.com <archivojab@gmail.com@2a67cc54-6c24-2940-3a0d-856ca1dc6a15>
2011-11-10 23:00:01 +0000

    I have added a second example to show how to use Intents and Buttons

---
+++ jabrena/android/intents/src/jab/android/intents/IntentsActivity.java
@@ -0,0 +1,60 @@
+package jab.android.intents;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class IntentsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        //Programación de boton
+        
+        Button btnLanzar = (Button)findViewById(R.id.button1);
+        btnLanzar.setOnClickListener(new OnClickListener() {
+
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				/*
+				 *Navegar por un sitio Web
+				 *  */
+				startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com/")));
+				
+				
+				/*
+				 *  Operar los contactos de mi directorio
+				startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("content://contacts/people/")));
+				*/
+				
+				/* 
+				 * Iniciar una llamada telefonica
+				startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("tel:34247")));
+				*/
+				
+				/*
+				 *  Realizar un busqueda en Google 
+				Intent intent= new Intent(Intent.ACTION_WEB_SEARCH );
+				intent.putExtra(SearchManager.QUERY, "PEAC Madrid");
+				startActivity(intent);
+				*/
+				
+				/*
+				 *  Lanzamiento de un comando de voz
+				startActivity(new Intent(Intent.ACTION_VOICE_COMMAND));
+				*/
+				
+			}
+		});
+    }
+}
