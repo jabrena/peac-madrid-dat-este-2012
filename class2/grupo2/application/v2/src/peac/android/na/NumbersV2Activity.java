f765ba9f7576cf94033f14c072ee0469ab5e2305
archivojab@gmail.com <archivojab@gmail.com@2a67cc54-6c24-2940-3a0d-856ca1dc6a15>
2012-01-14 10:48:25 +0000

    New update

--- class2/grupo2/application/v2/src/peac/android/na/NumbersV2Activity.java
+++ class2/grupo2/application/v2/src/peac/android/na/NumbersV2Activity.java
@@ -2,12 +2,27 @@ package peac.android.na;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class NumbersV2Activity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app);
+        setContentView(R.layout.main);
+        
+        //Boton
+      Button btnLanzar2 = (Button)findViewById(R.id.button);
+      btnLanzar2.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(NumbersV2Activity.this, "Ir a la pagina2", Toast.LENGTH_SHORT).show();
+			}
+      });
     }
+    
 }
